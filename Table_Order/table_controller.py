135d6baf4c7f710a8e762476f2d3d9573eab3eb1
HuynhNgoc <huynhngoc.bc@gmail.com>
2024-06-01 16:01:48 +0700

    create ui menu

--- Table_Order/table_controller.py
+++ Table_Order/table_controller.py
@@ -3,8 +3,12 @@ import tkinter as tk
 from tkinter import messagebox
 
 import peewee
+
+from Bill.bill_model import Billing
+from Table_Order.menu_food_controller import MenuFoodController
 from Table_Order.table_model import *
 from Table_Order.table_view import TableView
+from WareHouse.product_model import Product
 from database.connection import Connection
 from share.common_config import UserType
 
@@ -12,15 +16,18 @@ from share.common_config import UserType
 class TableController:
     def __init__(self, window):
         self.__data_table = []
-        self.__user_type = UserType.ADMIN
+        self.__user_type = UserType.NORMAL
         self.root = window
-        self.__get_data()
+        self.__get_table_data()
         self.view = TableView(window, self, self.__user_type)
 
     @property
     def tables(self):
         return self.__data_table
-    def __get_data(self):
+
+
+
+    def __get_table_data(self):
         self.__data_table = []
         try:
             Connection.db_handle.connect()
@@ -64,7 +71,18 @@ class TableController:
         finally:
             Connection.db_handle.close()
 
-
+    def update_table_status(self, id_table, status):
+        try:
+            Connection.db_handle.connect()
+            table = Table.get(Table.id == id)
+            table.status = status
+            table.save()
+            print("Update table success")
+        except peewee.InternalError as px:
+            print("Update table failure")
+            print(str(px))
+        finally:
+            Connection.db_handle.close()
 
     def __delete_table(self, table_id):
         try:
@@ -83,14 +101,41 @@ class TableController:
 
     def delete_and_reload(self, table_id):
         self.__delete_table(table_id)
-        self.__get_data()
+        self.__get_table_data()
 
 
     def add_new_and_reload(self, table_num_value, seat_num_value, status_value):
         self.add_new_table_to_db(table_num_value, seat_num_value, status_value)
-        self.__get_data()
+        self.__get_table_data()
 
     def update_and_reload(self, id, table_num_value, seat_num_value, status_value):
         self.update_table_to_db(id, table_num_value, seat_num_value, status_value)
-        self.__get_data()
+        self.__get_table_data()
+
+    def save_bill(self):
+        try:
+            Connection.db_handle.connect()
+            b = Billing()
+            b.save()
+            print("Save bill success")
+        except peewee.InternalError as px:
+            print("Update bill failure")
+            print(str(px))
+        finally:
+            Connection.db_handle.close()
+
+    def update_bill(self, id_bill, quantity):
+        try:
+            Connection.db_handle.connect()
+            b = Billing()
+            b.save()
+            print("Save bill success")
+        except peewee.InternalError as px:
+            print("Update bill failure")
+            print(str(px))
+        finally:
+            Connection.db_handle.close()
+
+    def show_menu_food(self, view_master):
+        menu = MenuFoodController(view_master)
 
